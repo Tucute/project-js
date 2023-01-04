5495cd1ce7fece93b0f7af30bac1b7c416e680f5
A An Tu <tupnv24@gmail.com>
2023-01-04 19:43:52 +0700

    add code

---
+++ data.js
@@ -0,0 +1,443 @@
+var tour = [
+    {
+        id: 41,
+        orign: "Sài Gòn",
+        destination: "Đà Lạt",
+        type: "giường",
+        distance: 310,
+        duration: "8 giờ",
+        price: 300000,
+        img:"https://kinhnghiemdulichkct.com/wp-content/uploads/2018/05/Da-lat-lam-dong-thanh-pho-xu-lanh.jpg" 
+    },
+    {
+        id: 42,
+        orign: "Sài Gòn",
+        destination: "Nha Trang",
+        type: "giường",
+        distance: 450,
+        duration: "9 giờ",
+        price: 400000,
+        img: "https://bcp.cdnchinhphu.vn/344443456812359680/2022/12/27/nhattrang3-16721128389061596602579.jpg"
+    },
+    {
+        id: 43,
+        orign: "Sài Gòn",
+        destination: "Đà Nẵng",
+        type: "giường",
+        distance: 980,
+        duration: "20 giờ",
+        price: 800000,
+        img: "https://vcdn1-dulich.vnecdn.net/2022/06/03/cauvang-1654247842-9403-1654247849.jpg?w=1200&h=0&q=100&dpr=1&fit=crop&s=Swd6JjpStebEzT6WARcoOA"
+    },
+    {
+        id: 44,
+        orign: "Sài Gòn",
+        destination: "Cần Thơ",
+        type: "giường",
+        distance: 190,
+        duration: "4 giờ",
+        price: 200000,
+        img: "https://cdn.vntrip.vn/cam-nang/wp-content/uploads/2017/08/1-3.jpg"
+    },
+    {
+        id: 45,
+        orign: "Đà Nẵng",
+        destination: "Hà Nội",
+        type: "giường",
+        distance: 740,
+        duration: "18 giờ",
+        price: 600000,
+        img: "https://ik.imagekit.io/tvlk/blog/2022/08/pho-co-ha-noi-11.jpg?tr=dpr-1.5,h-320,q-40,w-1024"
+    },
+    {
+        id: 46,
+        orign: "Sài Gòn",
+        destination: "Rạch Giá",
+        type: "giường",
+        distance: 230,
+        duration: "5 giờ",
+        price: 200000,
+        img: "https://thamhiemmekong.com/wp-content/uploads/2020/06/congtamquanrachgia.jpg"
+    },
+    {
+        id: 47,
+        orign: "Sài Gòn",
+        destination: "Châu Đốc",
+        type: "giường",
+        distance: 240,
+        duration: "6 giờ",
+        price: 200000,
+        img: "https://nemtv.vn/wp-content/uploads/2019/03/ch%C3%B9a-ba-doc-nemtv-2.jpg"
+    },
+    {
+        id: 48,
+        orign: "Sài Gòn",
+        destination: "Cà Mau",
+        type: "giường",
+        distance: 357,
+        duration: "8 giờ",
+        price: 300000,
+        img: "https://ik.imagekit.io/tvlk/blog/2021/10/du-lich-ca-mau-1-1024x573.jpg"
+    },
+    {
+        id: 1,
+        orign: "An Nhơn",
+        destination: "TP.Ho Chi Minh",
+        type: "giường",
+        distance: 660,
+        duration: "12 giờ 46 phút",
+        price: 300000
+    },
+    {
+        id: 2,
+        orign: "Bạc Liêu",
+        destination: "TP.Ho Chi Minh",
+        type: "giường",
+        distance: 271,
+        duration: "6 giờ",
+        price: 260000
+    },
+    {
+        id: 3,
+        orign: "Bảo Lộc",
+        destination: "Huế",
+        type: "giường",
+        distance: 827,
+        duration: "19 giờ",
+        price: 500000
+    },
+    {
+        id: 4,
+        orign: "Bảo Lộc",
+        destination: "Đà Nẵng",
+        type: "giường",
+        distance: 756,
+        duration: "15 giờ 38 phút",
+        price: 460000
+    },
+    {
+        id: 5,
+        orign: "Bến Tre",
+        destination: "TP.Ho Chi Minh",
+        type: "ghế",
+        distance: 75,
+        duration: "2 giờ",
+        price: 100000
+    },
+    {
+        id: 6,
+        orign: "Bình Dương",
+        destination: "Cà Mau",
+        type: "ghế",
+        distance: 317,
+        duration: "6 giờ 21 phút",
+        price: 310000
+    },
+    {
+        id: 7,
+        orign: "Bình Dương",
+        destination: "Rạch Giá",
+        type: "ghế",
+        distance: 272,
+        duration: "5 giờ 35 phút",
+        price: 270000
+    },
+    {
+        id: 8,
+        orign: "Bình Dương",
+        destination: "Hà Tiên",
+        type: "ghế",
+        distance: 362,
+        duration: "7 giờ 30 phút",
+        price: 350000
+    },
+    {
+        id: 9,
+        orign: "Bình Dương",
+        destination: "Sóc Trăng",
+        type: "ghế",
+        distance: 255,
+        duration: "5 giờ 26 phút",
+        price: 250000
+    },
+    {
+        id: 10,
+        orign: "Bình Dương",
+        destination: "Cà Mau",
+        type: "giường",
+        distance: 380,
+        duration: "6 giờ 37 phút",
+        price: 380000
+    },
+    {
+        id: 11,
+        orign: "Đà Nẵng",
+        destination: "Quảng Nam",
+        type: "giường",
+        distance: 80,
+        duration: "1 giờ 10 phút",
+        price: 100000
+    },
+    {
+        id: 12,
+        orign: "Đà Nẵng",
+        destination: "Ngãng Ngãi",
+        type: "giường",
+        distance: 112,
+        duration: "1 giờ 36 phút",
+        price: 110000
+    },
+    {
+        id: 13,
+        orign: "Đà Nẵng",
+        destination: "Kon Tum",
+        type: "giường",
+        distance: 270,
+        duration: "4 giờ 30 phút",
+        price: 250000
+    },
+    {
+        id: 14,
+        orign: "Đà Nẵng",
+        destination: "TP.Ho Chi Minh",
+        type: "giường",
+        distance: 957,
+        duration: "16 giờ 48 phút",
+        price: 900000
+    },
+    {
+        id: 15,
+        orign: "Đà Nẵng",
+        destination: "Bình Định",
+        type: "giường",
+        distance: 300,
+        duration: "5 giờ",
+        price: 300000
+    },
+    {
+        id: 16,
+        orign: "Đà Nẵng",
+        destination: "Quảng Trị",
+        type: "giường",
+        distance: 300,
+        duration: "12 giờ 46 phút",
+        price: 300000
+    },
+    {
+        id: 17,
+        orign: "Quảng Ngãi",
+        destination: "TP.Ho Chi Minh",
+        type: "giường",
+        distance: 230,
+        duration: "6 giờ",
+        price: 220000
+    },
+    {
+        id: 18,
+        orign: "Đăk Nông",
+        destination: "Đà Nẵng",
+        type: "giường",
+        distance: 636,
+        duration: "12 giờ 12 phút",
+        price: 600000
+    },
+    {
+        id: 19,
+        orign: "Hà Nội",
+        destination: "Đà Nẵng",
+        type: "giường",
+        distance: 763,
+        duration: "13 giờ 38 phút",
+        price: 700000
+    },
+    {
+        id: 20,
+        orign: "Hà Nội",
+        destination: "Nam Định",
+        type: "ghế",
+        distance: 78,
+        duration: "1 giờ 30 phút",
+        price: 100000
+    },
+    {
+        id: 21,
+        orign: "Huế",
+        destination: "Đà Lạt",
+        type: "giường",
+        distance: 745,
+        duration: "15 giờ 35 phút",
+        price: 750000
+    },
+    {
+        id: 22,
+        orign: "Huế",
+        destination: "TP.Ho Chi Minh",
+        type: "giường",
+        distance: 942,
+        duration: "25 giờ",
+        price: 850000
+    },
+    {
+        id: 23,
+        orign: "Huế",
+        destination: "Hà Nội",
+        type: "giường",
+        distance: 654,
+        duration: "11 giờ 50 phút",
+        price: 600000
+    },
+    {
+        id: 24,
+        orign: "Huế",
+        destination: "Bảo Lộc",
+        type: "giường",
+        distance: 826,
+        duration: "19 giờ",
+        price: 800000
+    },
+    {
+        id: 25,
+        orign: "Nam Định",
+        destination: "Hà Nội",
+        type: "ghế",
+        distance: 77,
+        duration: "1 giờ 30 phút",
+        price: 100000
+    },
+    {
+        id: 26,
+        orign: "Nha Trang",
+        destination: "Buôn Ma Thuật",
+        type: "giường",
+        distance: 470,
+        duration: "8 giờ 10 phút",
+        price: 410000
+    },
+    {
+        id: 27,
+        orign: "Nha Trang",
+        destination: "Đà Nẵng",
+        type: "giường",
+        distance: 528,
+        duration: "9 giờ 25 phút",
+        price: 500000
+    },
+    {
+        id: 28,
+        orign: "Nha Trang",
+        destination: "Đà Lạt",
+        type: "giường",
+        distance: 137,
+        duration: "4 giờ",
+        price: 130000
+    },
+    {
+        id: 29,
+        orign: "Nha Trang",
+        destination: "Tp.Hồ Chí Minh",
+        type: "giường",
+        distance: 426,
+        duration: "11 giờ",
+        price: 400000
+    },
+    {
+        id: 30,
+        orign: "Qui Nhơn",
+        destination: "TP.Hồ Chí Minh",
+        type: "giường",
+        distance: 643,
+        duration: "11 giờ 37 phút",
+        price: 640000
+    },
+    {
+        id: 31,
+        orign: "Qui Nhơn",
+        destination: "Đà Nẵng",
+        type: "giường",
+        distance: 500,
+        duration: "9 giờ 10 phút",
+        price: 500000
+    },
+    {
+        id: 32,
+        orign: "Sóc Trăng",
+        destination: "TP.Hồ Chí Minh",
+        type: "giường",
+        distance: 222,
+        duration: "5 giờ 36 phút",
+        price: 220000
+    },
+    {
+        id: 33,
+        orign: "TP.Hồ Chí Minh",
+        destination: "Đà Lạt",
+        type: "giường",
+        distance: 305,
+        duration: "8 giờ",
+        price: 300000
+    },
+    {
+        id: 34,
+        orign: "TP.Hồ Chí Minh",
+        destination: "Trà Ôn",
+        type: "giường",
+        distance: 161,
+        duration: "3 giờ 16 phút",
+        price: 150000
+    },
+    {
+        id: 35,
+        orign: "TP.Hồ Chí Minh",
+        destination: "Cần Thơ",
+        type: "giường",
+        distance: 166,
+        duration: "3 giờ 12 phút",
+        price: 160000
+    },
+    {
+        id: 36,
+        orign: "TP.Hồ Chí Minh",
+        destination: "Phan Rang",
+        type: "giường",
+        distance: 326,
+        duration: "7 giờ",
+        price: 320000
+    },
+    {
+        id: 37,
+        orign: "TP.Hồ Chí Minh",
+        destination: "Vĩnh Châu",
+        type: "giường",
+        distance: 247,
+        duration: "4 giờ 51 phút",
+        price: 240000
+    },
+    {
+        id: 38,
+        orign: "TP.Hồ Chí Minh",
+        destination: "Tây Ninh",
+        type: "giường",
+        distance: 121,
+        duration: "2 giờ 50 phút",
+        price: 120000
+    },
+    {
+        id: 39,
+        orign: "TP.Hồ Chí Minh",
+        destination: "Huế",
+        type: "giường",
+        distance: 1042,
+        duration: "18 giờ 25 phút",
+        price: 900000
+    },
+    {
+        id: 40,
+        orign: "TP.Hồ Chí Minh",
+        destination: "Đà Nẵng",
+        type: "giường",
+        distance: 957,
+        duration: "16 giờ 48 phút",
+        price: 850000
+    },
+
+]
---
+++ function.js
@@ -0,0 +1,80 @@
+function Save() {
+    localStorage.setItem('litstPlace', JSON.stringify(tour));
+}
+function Load() {
+    tour = JSON.parse(localStorage.getItem('listPlace'));
+}
+
+Load();
+Save();
+ 
+function selectPlace() {
+    var listplace = '';
+    for (var i in tour) {
+        var data = JSON.parse(JSON.stringify(tour[i]));
+        listplace += '<button type="button" class="list-group-item list-group-item-action" onclick="selectItem('+ data.orign +')" >' + data.orign + '</button>';
+    }
+    document.getElementById("listData").innerHTML = listplace;
+};
+
+function selectItem(i) {
+    console.log(i);
+    document.getElementById("select-origin").innerHTML=i;
+};
+
+function selectDestination() {
+    var destination = '' ;
+    var start = document.getElementById("select-origin").value; 
+    for (var i in tour) {
+        var data = JSON.parse(JSON.stringify(tour[i]));
+        if (start == data.orign) {
+            destination += '<button type="button" class="list-group-item list-group-item-action" onclick="selectItem('+ data.destination +')" >' + data.destination + '</button>';
+        }
+        document.getElementById("listDestination").innerHTML = destination;
+    }
+}
+
+var ShowPopulorTour = function() {
+    var listroutes = '';
+    for (var j in tour) {
+        var data = JSON.parse(JSON.stringify(tour[j]));
+        if (data.id > 40) {
+            listroutes += '<div class="col-sm-4 col-xs-12 col-ms-12 col-md-6 item-routes ">';
+            listroutes += '<button class="btn-items">';
+            listroutes += '<img src="' + data.img + '" class="image" style="min-width: 160px; min-height:76px">';
+            listroutes += '<div class="body-items">';
+            listroutes += '<p class="title">' + data.orign + ' ⇒ ' + data.destination + '</p>';
+            listroutes += '<div class="details-item">';
+            listroutes += '<div class="details">';
+            listroutes += '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-coin" viewBox="0 0 16 16">';
+            listroutes += '<path d="M5.5 9.511c.076.954.83 1.697 2.182 1.785V12h.6v-.709c1.4-.098 2.218-.846 2.218-1.932 0-.987-.626-1.496-1.745-1.76l-.473-.112V5.57c.6.068.982.396 1.074.85h1.052c-.076-.919-.864-1.638-2.126-1.716V4h-.6v.719c-1.195.117-2.01.836-2.01 1.853 0 .9.606 1.472 1.613 1.707l.397.098v2.034c-.615-.093-1.022-.43-1.114-.9H5.5zm2.177-2.166c-.59-.137-.91-.416-.91-.836 0-.47.345-.822.915-.925v1.76h-.005zm.692 1.193c.717.166 1.048.435 1.048.91 0 .542-.412.914-1.135.982V8.518l.087.02z"/>';
+            listroutes += '<path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>';
+            listroutes += '<path d="M8 13.5a5.5 5.5 0 1 1 0-11 5.5 5.5 0 0 1 0 11zm0 .5A6 6 0 1 0 8 2a6 6 0 0 0 0 12z"/>';
+            listroutes += '</svg>';
+            listroutes += '<span class="item-price">' + data.price + ' đ </span>';
+            listroutes += '</div>';
+            listroutes += '<div class="details">';
+            listroutes += '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clock" viewBox="0 0 16 16">';
+            listroutes += '<path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z"/>';
+            listroutes += '<path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0z"/>';
+            listroutes += '</svg>';
+            listroutes += '<span class="item-time">' + data.duration + '</span>';
+            listroutes += '</div>';
+            listroutes += '<div class="details">';
+            listroutes += '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">';
+            listroutes += '<path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>';
+            listroutes += '</svg>';
+            listroutes += '<span class="item-time">' + data.distance + ' km </span>';
+            listroutes += '</div>';
+            listroutes += '</div>';
+            listroutes += '</div>';
+            listroutes += '</button>';
+            listroutes += '</div>';
+
+        }
+    }
+    document.getElementById("list-popular-routes").innerHTML = listroutes;
+}
+
+ShowPopulorTour();
+
